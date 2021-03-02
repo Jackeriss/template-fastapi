8a56a4df5c3ec5b0122f938cff7c014dd626be0c
yufeizhao <yufeizhao@clubfactory.com>
2021-03-02 19:15:00 +0800

    update

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import example
+from app.api.api_v1.endpoints import example, ping
 
 api_router = APIRouter()
 api_router.include_router(example.router, prefix="/example", tags=["example"])
+api_router.include_router(ping.router, prefix="/ping", tags=["ping"])
--- app/api/api_v1/endpoints/ping.py
+++ app/api/api_v1/endpoints/ping.py
@@ -8,5 +8,5 @@ router = APIRouter()
 
 
 @router.get("/")
-async def read_items() -> Any:
+async def ping() -> Any:
     return "pong"
